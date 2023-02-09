89ac2901bceae41dd06dc5770a4066d04504a081
joao victor gomes <joao-iccz@hotmail.com>
2023-02-09 10:20:13 -0300

    Teste

---
+++ testes/salvarsemnome.test.js
@@ -0,0 +1,13 @@
+const Tarefa = require('../models/tarefa');
+jest.setTimeout(50000);
+describe('Tarefa', () => {
+  it('should not be able to create a task without a name', async () => {
+    const tarefa = new Tarefa({ conteudo: 'Example content' });
+
+    try {
+      await tarefa.save();
+    } catch (err) {
+      expect(err.message).toMatch(/tarefa validation failed: nome: Path `nome` is required./);
+    }
+  });
+});
