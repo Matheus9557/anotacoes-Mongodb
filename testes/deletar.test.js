b91578d719e45ab4eec1acfa3818993ad99b8391
joao victor gomes <joao-iccz@hotmail.com>
2023-02-13 14:43:41 -0300

    Ajustes

--- testes/deletar.test.js
+++ testes/deletar.test.js
@@ -1,12 +1,12 @@
 const deletarTarefa = require('../controllers/TarefaController').deletarTarefa;
 describe("Teste de remoção de tarefa", () => {
   it("Deve remover uma tarefa existente", () => {
-  const tarefasAntigas = [{ nome: "Fazer compras" }];
+  const tarefasAntigas = [{ nome: "Fazer compra" }];
   expect(tarefasAntigas).toHaveLength(1);
   
 
   const tarefasAtualizadas = tarefasAntigas.filter(
-    tarefa => tarefa.nome !== "Fazer compras"
+    tarefa => tarefa.nome !== "Fazer compra"
   );
   expect(tarefasAtualizadas).toHaveLength(0);
   });
