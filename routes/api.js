f2621c8e6b9109a8cdfa2a3e86d64e54ad159c79
Matheus Gomes <79982182+Matheus9557@users.noreply.github.com>
2023-02-14 16:54:37 -0300

    atualizando rotas

--- routes/api.js
+++ routes/api.js
@@ -3,14 +3,25 @@ require('dotenv').config();
 const express = require ('express');
 const router = express.Router();
 
+const loginController = require('../controllers/login');
 const tarefaController = require("../controllers/TarefaController");
-const Tarefa = require('../models/tarefa');
+
 
 router.get('/', tarefaController.getTarefas);
 
 router.get('/buscar/:nome', tarefaController.buscarTarefa);
 
+router.post('/pessoas', tarefaController.addPessoa);
+
+router.get('/pessoas', tarefaController.getPessoas);
+
+router.post('/login', loginController.createSession);
+
 router.post('/addtarefas', tarefaController.addTarefa);
+
+router.post('/postar', tarefaController.addPostagem);
+
+router.get('/postagens', tarefaController.getPostagens);
 
 router.delete('/deletar/:nome', tarefaController.deletarTarefa);
 
